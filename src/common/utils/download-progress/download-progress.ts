f7e0654fe7e192c7da3d25174568289686630186
vgorkavenko <vladimir.g@lido.fi>
2024-04-04 12:05:31 +0400

    feat: progress bar -> spinner

--- src/common/utils/download-progress/download-progress.ts
+++ src/common/utils/download-progress/download-progress.ts
@@ -1,67 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { MultiBar, Presets, SingleBar } from 'cli-progress';
+import ora, { Ora } from 'ora';
 import { IncomingHttpHeaders } from 'undici/types/header';
 import BodyReadable from 'undici/types/readable';
 
 @Injectable()
 export class DownloadProgress {
-  private multibar: MultiBar;
+  private spinner: Ora;
 
   constructor() {}
-
-  // TODO: how it works when error occurs from other promises in the same time ?
 
   public show(name: string, resp: { body: BodyReadable; headers: IncomingHttpHeaders }): void {
     const totalContentLength = Number(resp.headers['content-length']);
     let downloaded = 0;
     let speed = '0.00';
     let downloadedMb = 0;
-    let ratio = 0;
     const start = Date.now();
-    const bar = this.add(name, Number((totalContentLength / 1024 / 1024).toFixed(2)));
+    const dataSize = Number((totalContentLength / 1024 / 1024).toFixed(2));
+    this.add(name, dataSize);
 
     resp.body.on('data', (chunk) => {
       downloaded += chunk.length;
-      ratio = downloaded / totalContentLength;
       downloadedMb = Number(downloaded / 1024 / 1024);
       speed = (downloadedMb / ((Date.now() - start) / 1000)).toFixed(2);
-      bar.update(ratio, {
-        speed,
-        downloaded: downloadedMb.toFixed(2),
-        status: '⤵️ ',
-      });
+      this.spinner.text = `${name} | ${downloadedMb.toFixed(2)} MB / ${dataSize} MB | ${speed} MB/s`;
     });
-    resp.body.on('end', () => renderFinish('✅ Downloaded\n'));
-    resp.body.on('error', () => renderFinish('❌ Failed\n'));
+    resp.body.on('end', () => this.spinner.succeed());
+    resp.body.on('error', () => this.spinner.fail());
+  }
 
-    const renderFinish = (status: string) => {
-      bar.update(ratio, {
-        speed,
-        downloaded: downloadedMb.toFixed(2),
-        status,
+  private add(name: string, size: number) {
+    if (!this.spinner) {
+      this.spinner = ora({
+        text: `${name} | 0 MB / ${size} MB | 0.00 MB/s️`,
+        spinner: 'dots',
       });
-      bar.render();
-      bar.stop();
-    };
-  }
-
-  private add(name: string, size: number): SingleBar {
-    if (!this.multibar) {
-      this.initMultibar();
     }
-    return this.multibar.create(1, 0, { name, size });
-  }
-
-  private initMultibar(): void {
-    this.multibar = new MultiBar(
-      {
-        fps: 1,
-        hideCursor: true,
-        noTTYOutput: true,
-        emptyOnZero: true,
-        format: ` | {name} |{bar}| {percentage}% || {downloaded} of {size} Mb | Speed: {speed} Mb/s | {status}`,
-      },
-      Presets.shades_grey,
-    );
+    this.spinner.start();
   }
 }
