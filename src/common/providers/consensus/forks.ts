01840ec19fc2793d28ae163eedf01aeab35aa5c2
vgorkavenko <vladimir.g@lido.fi>
2026-06-15 15:46:32 +0200

    chore: remove dead code

--- src/common/providers/consensus/forks.ts
+++ src/common/providers/consensus/forks.ts
@@ -2,12 +2,9 @@ import { ForkName } from '@lodestar/params';
 import { sszTypesFor } from '@lodestar/types';
 
 const SUPPORTED_FORKS = [ForkName.capella, ForkName.deneb, ForkName.electra, ForkName.fulu] as const;
-const POST_ELECTRA_FORKS = [ForkName.electra, ForkName.fulu] as const;
 
 export type SupportedForkKey = (typeof SUPPORTED_FORKS)[number];
-export type PostElectraForkKey = (typeof POST_ELECTRA_FORKS)[number];
 type ForkSsz = ReturnType<typeof sszTypesFor<SupportedForkKey>>;
-type PostElectraForkSsz = ReturnType<typeof sszTypesFor<PostElectraForkKey>>;
 
 export type SupportedBlock = ReturnType<ForkSsz['BeaconBlock']['fromJson']>;
 export type SupportedBlockView = ReturnType<ForkSsz['BeaconBlock']['toView']>;
@@ -22,12 +19,6 @@ export function parseFork(forkName: string): SupportedForkKey {
   return forkName as SupportedForkKey;
 }
 
-export function isPostElectraFork(forkName: SupportedForkKey): forkName is PostElectraForkKey {
-  return (POST_ELECTRA_FORKS as readonly string[]).includes(forkName);
-}
-
-export function getSsz(forkName: PostElectraForkKey): PostElectraForkSsz;
-export function getSsz(forkName: SupportedForkKey): ForkSsz;
 export function getSsz(forkName: SupportedForkKey): ForkSsz {
   return sszTypesFor(forkName);
 }
