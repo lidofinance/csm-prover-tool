0db10ff09d3425d81a0ea8143a83fdc06aba521e
vgorkavenko <Vladimir.G@lido.fi>
2026-07-21 10:29:57 +0200

    feat: add dockerignore + config logging

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -1,16 +1,28 @@
 import { ConfigService as ConfigServiceSource } from '@nestjs/config';
+import { plainToInstance } from 'class-transformer';
 
-import { type EnvironmentVariables } from './env.validation.js';
+import { EnvironmentVariables } from './env.validation.js';
 
 export class ConfigService extends ConfigServiceSource<EnvironmentVariables> {
   /**
    * List of env variables that should be hidden
    */
   public get secrets(): string[] {
-    return [...this.get('EL_RPC_URLS'), ...this.get('CL_API_URLS'), ...(this.get('KEYSAPI_API_URLS') ?? [])];
+    return [
+      this.get('TX_SIGNER_PRIVATE_KEY'),
+      ...this.get('EL_RPC_URLS'),
+      ...this.get('CL_API_URLS'),
+      ...(this.get('KEYSAPI_API_URLS') ?? []),
+    ].filter(Boolean);
   }
 
   public get<T extends keyof EnvironmentVariables>(key: T): EnvironmentVariables[T] {
     return super.get(key, { infer: true }) as EnvironmentVariables[T];
+  }
+
+  /** App config snapshot for the startup ENV dump; secret values are masked by the log transport (see `secrets`). */
+  public snapshot(): Record<string, unknown> {
+    const keys = Object.keys(plainToInstance(EnvironmentVariables, process.env)) as (keyof EnvironmentVariables)[];
+    return Object.fromEntries(keys.map((key) => [key, this.get(key)]));
   }
 }
